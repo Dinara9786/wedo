dbc94654d751b108845c546c5e37ad21eb00e18d
Dinara9786 <knowledge9786@gmail.com>
2020-11-27 20:58:57 -0500

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        keeping track of our first project
+                we have made our first commit and made the first commit
     }
 }
